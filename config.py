0638a895aea3968c21ae02293996c689b32dc4f2
xDorito <xdorito@gmail.com>
2025-06-09 16:32:31 -0400

    Configuration refactor initial commit

--- config.py
+++ config.py
@@ -1,39 +1,179 @@
 import discord
+import os
 from datetime import time
 from zoneinfo import ZoneInfo
+from dotenv import load_dotenv
 
+# lets use dotenv to load environment variables
+# and a role manager to return dev or prod roles
+load_dotenv()
+CURRENT_ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-# Guild/Server related constants
-GUILD_ID = discord.Object(id=1267584422253694996)
-GUILD_ID_INT = 1267584422253694996
-GUILD_MEMBER_ID = 1268739778119995505
-LEGION_ID = 1267584422253694996
+DISCORD_TOKEN = os.getenv('BOT_TOKEN')
 
-# Role IDs
-SENATOR_ROLE = 1311824922301038632
-ADMINISTRATOR_ROLE = 1371792303206699040
-ADVERTISER_ROLE_ID = 1360478811661144114
-TICKET_ROLE_ID = 1324782094571798621
-DISCIPLINARY_ROLES = [1367304039137542155, 1367304098659176641]
+DEVELOPEMENT = {
+    "GUILD": {
+        "id": 1267584422253694996,
+        "object": discord.Object(id=1267584422253694996, type=discord.Guild),
+    },
+    "MEMBER": {
+        "id": 1268739778119995505,
+        "name": "Legionnaire"
+    },
+    "SENETOR": {
+        "id": 1311824922301038632,
+        "name": "Senator"
+    },
+    "ADMINISTRATOR": {
+        "id": 1371792303206699040,
+        "name": "Princeps"
+    },
+    "ADVERTISER": {
+        "id": 1360478811661144114,
+        "name": "Legion Advertiser"
+    },
+    "TICKET": {
+        "id": 1324782094571798621,
+        "name": "Ticket TIME"
+    },
+    "DISCIPLINARY": {
+        "ids": [1367304039137542155, 1367304098659176641],
+        "names": ["Bonked", "Pecunaria"]  # idk what these actually are
+    },
+    "PROFFESIONS": {
+        "foraging": {
+            "name": "Foraging",
+            "id": 1267585190981796021
+        },
+        "hunting": {
+            "name": "Hunting",
+            "id": 1267585359986823168
+        },
+        "mining": {
+            "name": "Mining",
+            "id": 1267585386859728927
+        },
+        "forestry": {
+            "name": "Forestry",
+            "id": 1267585920773918752
+        },
+        "carpentry": {
+            "name": "Carpentry",
+            "id": 1267585436885323817
+        },
+        "leatherworking": {
+            "name": "Leatherworking",
+            "id": 1267585503385882665
+        },
+        "masonry": {
+            "name": "Masonry",
+            "id": 1267585535858315306
+        },
+        "smithing": {
+            "name": "Smithing",
+            "id": 1267585569442238556
+        },
+        "tailoring": {
+            "name": "Tailoring",
+            "id": 1267585594750402581
+        },
+        "scholar": {
+            "name": "Scholar",
+            "id": 1267585753496551627
+        },
+        "farming": {
+            "name": "Farming",
+            "id": 1267585784404377763
+        },
+        "fishing": {
+            "name": "Fishing",
+            "id": 1267585808186081311
+        }
+    }
+}
+PRODUCTION = {
+    "GUILD": {
+        "id": 1267584422253694996,
+        "object": discord.Object(id=1267584422253694996, type=discord.Guild),
+    },
+    "MEMBER": {
+        "id": 1268739778119995505,
+        "name": "Legionnaire"
+    },
+    "SENETOR": {
+        "id": 1311824922301038632,
+        "name": "Senator"
+    },
+    "ADMINISTRATOR": {
+        "id": 1371792303206699040,
+        "name": "Princeps"
+    },
+    "ADVERTISER": {
+        "id": 1360478811661144114,
+        "name": "Legion Advertiser"
+    },
+    "TICKET": {
+        "id": 1324782094571798621,
+        "name": "Ticket TIME"
+    },
+    "DISCIPLINARY": {
+        "ids": [1367304039137542155, 1367304098659176641],
+        "names": ["Bonked", "Pecunaria"]  # idk what these actually are
+    },
+    "PROFFESIONS": {
+        "foraging": {
+            "name": "Foraging",
+            "id": 1267585190981796021
+        },
+        "hunting": {
+            "name": "Hunting",
+            "id": 1267585359986823168
+        },
+        "mining": {
+            "name": "Mining",
+            "id": 1267585386859728927
+        },
+        "forestry": {
+            "name": "Forestry",
+            "id": 1267585920773918752
+        },
+        "carpentry": {
+            "name": "Carpentry",
+            "id": 1267585436885323817
+        },
+        "leatherworking": {
+            "name": "Leatherworking",
+            "id": 1267585503385882665
+        },
+        "masonry": {
+            "name": "Masonry",
+            "id": 1267585535858315306
+        },
+        "smithing": {
+            "name": "Smithing",
+            "id": 1267585569442238556
+        },
+        "tailoring": {
+            "name": "Tailoring",
+            "id": 1267585594750402581
+        },
+        "scholar": {
+            "name": "Scholar",
+            "id": 1267585753496551627
+        },
+        "farming": {
+            "name": "Farming",
+            "id": 1267585784404377763
+        },
+        "fishing": {
+            "name": "Fishing",
+            "id": 1267585808186081311
+        }
+    }
+}
 
 # Time settings
 ANNOY_TIME = time(hour=0, minute=0, second=0, tzinfo=ZoneInfo("America/Chicago"))
-
-# Profession roles
-PROFESSION_ROLES = {
-    "foraging": 1267585190981796021,
-    "hunting": 1267585359986823168,
-    "mining": 1267585386859728927,
-    "forestry": 1267585920773918752,
-    "carpentry": 1267585436885323817,
-    "leatherworking": 1267585503385882665,
-    "masonry": 1267585535858315306,
-    "smithing": 1267585569442238556,
-    "tailoring": 1267585594750402581,
-    "scholar": 1267585753496551627,
-    "farming": 1267585784404377763,
-    "fishing": 1267585808186081311
-}
 
 # Bot settings
 BOT_PREFIX = '$'
